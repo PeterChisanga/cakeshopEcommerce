0172da68a10d78752efb29098ce3752c25baea3c
PeterChisanga <peterchisangamwamba@gmail.com>
2023-03-09 05:53:00 +0100

    login done

---
+++ resources/views/login.blade.php
@@ -0,0 +1,22 @@
+@extends('master')
+@section('content')
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+            <form action="/login" method="POST">
+             @csrf
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                </div>
+                
+                <button type="submit" class="btn btn-default">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
